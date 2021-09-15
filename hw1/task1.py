e8cd8cb8591cec0f0ed327a8326fb6c947b96478
aarontravass <aaron.travass@hotmail.com>
2021-09-16 00:51:22 +0530

    COMPLETED TASK 1

---
+++ hw1/task1.py
@@ -0,0 +1,19 @@
+protien_seq = "ACDE"
+
+
+def proteinCheck(s: str):
+    for pr in protien_seq:
+        if s.count(pr) <= 0:
+            print("Not a protein sequence")
+            return False
+    print("This is a protein sequence")
+    return True
+
+
+def aminoAcidCalc(s):
+    if not proteinCheck(s):
+        return
+    print("Percentage of Acidic Amino acids is "+str((s.count('D')+s.count('E'))/len(s)))
+    return
+
+aminoAcidCalc(input("Input a sequence - "))
