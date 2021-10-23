75c7c5b9cf58b7f790a82b939baae5e55739be01
aarontravass <aaron.travass@hotmail.com>
2021-10-23 14:58:51 +0530

    done with hw4

--- hw4/task4.py
+++ hw4/task4.py
@@ -1,9 +1,8 @@
-def list_sum(L, i):
-    if (i < len(L)):
-        return L[i] + list_sum(L, i + 1)
-    return 0
-
 L = [i for i in range(0, 40)]
 
-even_list = [i + list_sum(L, i) for i in L]
-print(even_list)
+even_dict = {'sum': 0}
+odd_dict = {'sum': 0}
+even_list = [even_dict.update({'sum': (i + even_dict['sum'])}) for i in L if i % 2]
+odd_list = [odd_dict.update({'sum': (i + odd_dict['sum'])}) for i in L if not i % 2]
+print("sum of even numbers is",even_dict['sum'])
+print("sum of odd numbers is",odd_dict['sum'])
