b6019001b8328715c89f19bcbedad80f305bb0bd
aarontravass <aaron.travass@hotmail.com>
2021-10-25 17:54:00 +0530

    done with hw4

--- hw5/task1.py
+++ hw5/task1.py
@@ -1,3 +1,4 @@
-nums = [i for i in range(100, 999) if (i % 17 == 0)]
-print(len(nums), " number of 3 digit numbers are divisible by 17")
-print(nums)
+nums = round((999-100)/17)
+print(nums, "number of 3 digit numbers are divisible by 17")
+print([i for i in range(100,1000) if i%17==0])
+
