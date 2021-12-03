6b581ac9fa8bc79322031366659cb2f0779298cd
aarontravass <aaron.travass@hotmail.com>
2021-12-03 13:57:33 +0530

    completed t1

---
+++ hw7/t1.py
@@ -0,0 +1,15 @@
+def bubblesort(mylist):
+    num = len(mylist) - 1
+    for i in range(num):
+        shouldContinue = False
+        for j in range(num):
+            if mylist[j]>mylist[j+1]:
+                mylist[j], mylist[j+1] = mylist[j+1], mylist[j]
+                shouldContinue = True
+        print(i, mylist)
+        if not shouldContinue:
+            break
+
+a=[1,5,3,2]
+bubblesort(a)
+print(a)
